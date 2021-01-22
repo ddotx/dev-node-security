3d01ffe67f62c1962a37a912d5dda2b3e2bd9569
ddotx <domemacintosh@gmail.com>
2021-01-22 10:52:50 +0700

    init

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.status(200).json({
+        msg: 'success'
+    })
+})
+
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+    console.log(`listening on port ${PORT}`)
+})
